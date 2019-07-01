d2b02f1cc490143ef33b88f7cec66d4ca11ac4fa
unknown <sleon@lealtad360.com>
2019-07-01 00:47:05 -0400

    Delete cors

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const koaBetterBody = require('koa-better-body');
 const routerAuth = require('./routes/auth');
 const initDB = require('./config/db');
 const routerCustomer = require('./routes/customer');
-const corsRegla = require('./config/cors');
 
 
 const app = new Koa();
 
 initDB();
 app.use(koaBetterBody({fields: 'body'}));
-routerAuth.middleware(corsRegla);
 app.use(routerAuth.routes())
 app.use(routerCustomer.routes())
 app.use(routerAuth.allowedMethods())
